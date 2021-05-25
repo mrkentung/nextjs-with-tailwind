58bf98211c5113fc73b9d89012f36a34e1d3c6a5
mrkentung <widiyanto.adhi@gmail.com>
2021-05-25 17:31:49 +0700

    feat: support darkMode

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,59 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import Head from 'next/head';
 
 export default function Home() {
+  const selectTheme = (value) => {
+    localStorage.setItem('theme', value);
+    document.querySelector('html').classList.add(localStorage.getItem('theme'));
+
+    const html = document.querySelector('html');
+
+    if (value === 'dark') {
+      html.classList.remove('light');
+    } else {
+      html.classList.remove('dark');
+    }
+  };
+
+  useEffect(() => {
+    document.querySelector('html').classList.add(localStorage.getItem('theme'));
+  }, []);
+
   return (
     <Fragment>
       <Head>
         <title>Home</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="md:flex bg-white rounded-lg p-24 justify-center">
-        <img
-          className="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
-          src="https://pbs.twimg.com/profile_images/595885543474401280/LbeFDcwr_400x400.jpg"
-          alt="profile pict"
-        />
-        <div className="text-center md:text-left">
-          <h2 className="text-lg">Adhi Widiyanto</h2>
-          <div className="text-purple-500">JavaScript developer</div>
-          <div className="text-gray-600">Twitter: @mrkentunggg</div>
-          <div className="text-gray-600">tawadancanda@gmail.com</div>
+      <div className="antialiased flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-700">
+        <div className="w-5/12">
+          <div className="bg-white dark:bg-gray-800 shadow p-4 rounded-lg mb-4 flex items-center justify-between">
+            <div className="font-semibold text-lg text-gray-800 dark:text-white">Switcher</div>
+            <div>
+              <button
+                onClick={() => selectTheme('light')}
+                className="focus:outline-none w-4 h-4 bg-gray-200 rounded-full mr-2"></button>
+              <button
+                onClick={() => selectTheme('dark')}
+                className="focus:outline-none w-4 h-4 bg-black rounded-full mr-2"></button>
+            </div>
+          </div>
+          <div className="bg-white dark:bg-gray-800 shadow rounded-lg overflow-hidden">
+            <div className="px-10 py-8">
+              <h1 className="text-2xl font-bold text-gray-800 dark:text-white mb-6">
+                Nostrud laborum laborum exercitation aliqua nostrud mollit sit enim.
+              </h1>
+              <div className="leading-relaxed text-lg text-gray-500 dark:text-gray-200">
+                Cillum excepteur ea ea eiusmod adipisicing culpa minim dolor ex in esse duis ipsum
+                consectetur. Irure Lorem occaecat esse id do aute velit est deserunt fugiat. Fugiat
+                in dolor excepteur minim eiusmod.
+              </div>
+            </div>
+            <div className="px-10 py-4 bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-900 dark:text-gray-200">
+              Commodo nostrud sit adipisicing aliquip laboris ullamco et fugiat.
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="p-4 shadow rounded bg-white container mx-auto">
-        <h1 className="text-purple-500 leading-normal">Next.js</h1>
-        <p className="text-gray-500">with Tailwind CSS</p>
       </div>
     </Fragment>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
+  darkMode: 'class',
   purge: ['./pages/**/*.js', './components/**/*.js'],
   theme: {
     extend: {},
